a19466f8a7446b2866276715c718626cfd65dc2b
Safaa brouk <63406961+safaabrouk@users.noreply.github.com>
2024-06-08 18:20:46 +0100

    Add entities without ralation mapping

---
+++ oneToOneProject/src/main/java/com/myapp/entities/Adress.java
@@ -0,0 +1,25 @@
+package com.myapp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Adress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+
+}
---
+++ oneToOneProject/src/main/java/com/myapp/entities/User.java
@@ -0,0 +1,22 @@
+package com.myapp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "client") // Because there is default table has user as name
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
